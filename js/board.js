aab1ad0cf62e91e5d9e88f947f40379c254e3592
Alexandra <129203298+T-Alexandra@users.noreply.github.com>
2025-06-25 12:28:33 +0400

    added_allcoll_move

--- js/board.js
+++ js/board.js
@@ -43,6 +43,45 @@ class Board {
         }
     }
 
+     getEmptyInLine(index) {
+        const part = this.parts.find(p => p.index == index);
+        const empty = this.parts.find(p => p.value == 0);
+        if (part.row == empty.row) {
+            const rowParts = this.parts.filter(p => p.row == part.row);
+            const direction = part.col < empty.col ? 1 : -1;
+
+            for (let i = empty.col; i != part.col; i -= direction) {
+                const fromPart = rowParts.find(p => p.col == i - direction);
+                const toPart = rowParts.find(p => p.col == i);
+                toPart.value = fromPart.value;
+            }
+
+            part.value = 0;
+            
+        }
+        else if (part.col == empty.col) {
+            const colParts = this.parts.filter(p => p.col == part.col);
+            const direction = part.row < empty.row ? 1 : -1;
+
+            for (let i = empty.row; i != part.row; i -= direction) {
+                const fromPart = colParts.find(p => p.row == i - direction);
+                const toPart = colParts.find(p => p.row == i);
+                toPart.value = fromPart.value;
+            }
+
+            part.value = 0;
+        }
+        container.innerHTML = '';
+        for (const part of this.parts) {
+            const part1 = part.render();
+            part1.addEventListener('click', () => {
+                this.tryMoveClick(part.index, container);
+            });
+            container.appendChild(part1);
+        }
+        console.log("."); 
+    }
+    
     getNeighbors(index) {
         const neighbors = [];
         const part = this.parts.find(p => p.index == index);
@@ -74,7 +113,10 @@ class Board {
         const neighbors = this.getNeighbors(clickedIndex);
         const emptyNeighbor = neighbors.find(p => p.value == 0);
 
-        if (!emptyNeighbor) return;
+        if (!emptyNeighbor) {
+            this.getEmptyInLine(clickedIndex);
+            return;
+        };
 
         const a = clickedPart.value;
         clickedPart.value = emptyNeighbor.value;
